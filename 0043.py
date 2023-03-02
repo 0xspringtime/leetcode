eb56a7f1852c3c672530bc160802edb28431877f
0xspringtime <0xspringtime@protonmail.com>
2023-03-01 23:03:17 -0500

    commit

---
+++ 0043.py
@@ -0,0 +1,46 @@
+def multiply(num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += res[i1 + i2] // 10
+                res[i1 + i2] = res[i1 + i2] % 10
+
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
+
+
+def test():
+    test_cases = [
+        {
+            "name": "simple case 1",
+            "input": "2",
+            "input1": "3",
+            "expected": "6"
+        },
+        {
+            "name": "simple case 2",
+            "input": "123",
+            "input1": "456",
+            "expected": "56088"
+        },
+    ]
+
+    for test_case in test_cases:
+        assert test_case["expected"] == multiply(test_case["input"], test_case["input1"]), test_case["name"]
+
+if __name__ == "__main__":
+    from datetime import datetime
+    start_time = datetime.now()
+    test()
+    print("Everything passed")
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
