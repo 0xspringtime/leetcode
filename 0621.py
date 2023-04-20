ed93849926cdfc2aa966c00dc78d33b68110da40
0xspringtime <0xspringtime@protonmail.com>
2023-04-19 22:15:39 -0400

    commit

--- 0621.py
+++ 0621.py
@@ -1,20 +1,38 @@
-def average(L):
-    if not L:
-        return None
-    return sum(L)/len(L)
+from typing import List
+from collections import Counter, deque
+import heapq
+def leastInterval(tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()  # pairs of [-cnt, idleTime]
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
 
 def test():
     test_cases = [
         {
             "name": "simple case 1",
             "input": ["A","A","A","B","B","B"],
-            "input2": 2;
+            "input1": 2,
             "expected": 8 
         }
     ]
 
     for test_case in test_cases:
-        assert test_case["expected"] == average(test_case["input"], test_case["input1"]), test_case["name"]
+        assert test_case["expected"] == leastInterval(test_case["input"], test_case["input1"]), test_case["name"]
 
 if __name__ == "__main__":
     from datetime import datetime
