798568d89703acb9690f80f4d73eeff0d632138c
0xspringtime <0xspringtime@protonmail.com>
2023-06-23 10:38:34 -0400

    commit

---
+++ stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+#Push: Add an element to the top of the stack.
+#Pop: Remove an element from the top of the stack.
+#Peek/Top: Get the top element of the stack.
+#isEmpty: Check if the stack is empty.
+
