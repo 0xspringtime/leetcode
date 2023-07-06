1eeed086dfdb1eed0c88efcc0c51eed210696f4f
0xspringtime <0xspringtime@protonmail.com>
2023-07-06 07:37:51 -0400

    commit

---
+++ 1304n.py
@@ -0,0 +1,8 @@
+def sumZero(n):
+    # Generate numbers from -n//2 to n//2. Exclude zero if n is even.
+    result = list(range(-n//2, n//2 + 1))
+    if n % 2 == 0:
+        result.remove(0)
+    return result
+#time O(n)
+#space O(n)
