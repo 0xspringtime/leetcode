1d865038cfddd4846b6fbbaa460f7ec61386833f
0xspringtime <0xspringtime@protonmail.com>
2024-06-11 23:59:22 -0400

    commit

--- 0543n.py
+++ 0543n.py
@@ -1,10 +1,4 @@
 #DFS
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
 class Solution:
     def diameterOfBinaryTree(self, root):
